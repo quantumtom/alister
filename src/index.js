490457954f29f4f0946b4854335d78c2b9ac2978
quantumtom <cornyn@gmail.com>
2019-04-17 15:25:25 -0700

    Update component id.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Menu from './Menu';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// Put the thing into the DOM!
+ReactDOM.render(React.createElement(Menu), document.getElementById('menu'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
