c8c8cb2b18fbca34e0003bec40b499351574dcab
quantumtom <cornyn@gmail.com>
2019-04-29 11:55:02 -0700

    Add new React UI component.

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,28 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import './AppHeader.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const AppHeader = () => (
+  <AppBar position="static">
+    <Toolbar>
 
-export default App;
+    </Toolbar>
+  </AppBar>
+);
+
+export default AppHeader;
