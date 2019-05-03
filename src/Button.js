3e7d5589ca572e5b574696dc8160971f1700c40e
quantumtom <cornyn@gmail.com>
2019-05-03 13:30:31 -0700

    Add state functionality.

--- src/Button.js
+++ src/Button.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import './Button.css';
 
+function ButtonEnabled(props) {
+    return (
+        <div className="Button-wrapper">
+            <button className="Button">
+                {props.name}
+            </button>
+        </div>
+    );
+}
+
+function ButtonDisabled(props) {
+    return (
+        <div className="Button-wrapper">
+            <button className="Button" disabled="disabled">
+                {props.name}
+            </button>
+        </div>
+    );
+}
+
 class Button extends React.Component {
     constructor(props) {
         super(props);
+        this.name = 'Save';
         this.state = {
-            enabled: false,
+            enabled: true,
             value: null
         };
     }
 
     render() {
-        console.dir(this.state.enabled);
-        return (
-            <div className="Button-wrapper">
-                <button className="Button" {...() => {
-                    if (!this.state.enabled) {
-                        return "disabled=\"disabled\"";
-                    }
-                }} onClick={() => {
-                    this.setState({enabled: !(this.state.enabled)});
-                }}>
-                    {this.props.name}
-                </button>
-            </div>
-        );
+        console.log(this.state.enabled);
+        if (this.state.enabled) {
+            return <ButtonEnabled name={this.name} onClick={() => {
+                this.setState({enabled: false});
+            }
+            }/>;
+        } else {
+            return <ButtonDisabled name={this.name} onClick={() => {
+                this.setState({enabled: true});
+            }
+            }/>;
+        }
     }
 }
 
