e507394ca7b7332e8ad77825568bfaa9a6b0131d
quantumtom <cornyn@gmail.com>
2019-05-03 13:27:41 -0700

    Rename App to AppMain.

--- src/AppMain.test.js
+++ src/AppMain.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AppMain from './AppMain';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<AppMain />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
