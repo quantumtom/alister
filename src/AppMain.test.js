ebcd4bd0709cd9c8178ed1197240eb37bf3cb8be
quantumtom <cornyn@gmail.com>
2019-05-03 13:29:33 -0700

    Add AppMain module.

--- src/AppMain.test.js
+++ src/AppMain.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import AppMain from './AppMain';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AppMain />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<AppMain/>, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
